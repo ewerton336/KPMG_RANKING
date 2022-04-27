5454769ec218e0256cc77d07b8324c6c9082a0e5
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-04-27 18:16:08 -0300

    Início do projeto

---
+++ KPMG_RAKING/RankingAPIWeb/Controllers/GameResultController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankingAPIWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameResultController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveGameScore(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // GET: GameResultController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GameResultController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GameResultController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GameResultController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GameResultController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GameResultController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GameResultController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GameResultController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ KPMG_RAKING/RankingAPIWeb/Model/GameResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankingAPIWeb.Model
+{
+    public class GameResult
+    {
+        public int PlayerId { get; set; }
+        public long GameId { get; set; }
+        public int WinScore { get; set; }
+        public DateTime GameDate { get; set; }
+    }
+}
